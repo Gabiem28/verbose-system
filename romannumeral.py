43ad313a5df75e50d6b50eac14e7ba4f86826f55
Gabiem28 <93959035+Gabiem28@users.noreply.github.com>
2022-10-13 00:02:50 -0400

    Add files via upload

---
+++ romannumeral.py
@@ -0,0 +1,25 @@
+userNumber = int(input('Enter a number from 1 to 10:'))
+
+
+if userNumber == 1:
+    print ("The roman numeral for", userNumber, "is I.")
+elif userNumber == 2:
+    print ("The roman numeral for", userNumber, "is II.")
+elif userNumber == 3:
+    print ("The roman numeral for", userNumber, "is III.")
+elif userNumber == 4:
+    print ("The roman numeral for", userNumber, "is IV.")
+elif userNumber == 5:
+    print ("The roman numeral for", userNumber, "is V.")
+elif userNumber == 6:
+    print ("The roman numeral for", userNumber, "is VI.")
+elif userNumber == 7:
+    print ("The roman numeral for", userNumber, "is VII.")
+elif userNumber == 8:
+    print ("The roman numeral for", userNumber, "is VIII.")
+elif userNumber == 9:
+    print ("The roman numeral for", userNumber, "is IX.")
+elif userNumber == 10:
+    print ("The roman numeral for", userNumber, "is X.")
+else:
+    print ("Number must be between 1 and 10!")
