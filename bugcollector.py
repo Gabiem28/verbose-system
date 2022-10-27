242bbd655e4b558788917ead6f9efe4a6c06a6b6
Gabiem28 <93959035+Gabiem28@users.noreply.github.com>
2022-10-27 12:53:07 -0400

    Add files via upload

--- bugcollector.py
+++ bugcollector.py
@@ -1,16 +1,17 @@
 print ("Bug Collector")
 
 numOfBugs = 0
-DAYS = 5
+NUM_OF_DAYS = 5
 
 
 
-while numOfBugs < 1:
-    print ("Invalid. Number of bugs cannot be less than 0.")
-    numOfBugs = int(input('How many bugs did you collect today?: '))
 
-    for days in range(DAYS):
-        print ("How many bugs did you collect for day",(days+1))
-        numOfBugs = int(input())
+for days in range(NUM_OF_DAYS):
+    print ("How many bugs did you collect for day",(days+1),":", end=" ")
+    numOfBugs = int(input())
+
+    while numOfBugs < 1:
+        print ("Invalid. Number of bugs cannot be less than 0.")
+        numOfBugs = int(input('How many bugs did you collect for day'),(days+1))
 
     
